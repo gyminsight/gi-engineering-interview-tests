f38a6532a5217a33d8a7c52102f3dbef13e3c2df
Adolfo Fernández Lazo <fitolazoff@gmail.com>
2026-01-15 20:48:08 -0800

    - Creating Account model and DTOs for handling create, read, and update operations: Account model class, AccountCreateDto, AccountReadDto, AccountUpdateDto

---
+++ BackendDeveloperTest1/Test1/DTOs/AccountUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Test1.DTOs
+{
+    public class AccountUpdateDto
+    {
+        public Guid Guid { get; set; }
+        public uint LocationUid { get; set; }
+        public uint Status { get; set; }
+        public int AccountType { get; set; }
+        public DateTime? PeriodStartUtc { get; set; }
+        public DateTime? PeriodEndUtc { get; set; }
+        public DateTime? NextBillingUtc { get; set; }
+        public double? PaymentAmount { get; set; }
+        public bool PendCancel { get; set; }
+        public DateTime? PendCancelDateUtc { get; set; }
+        public DateTime? EndDateUtc { get; set; }
+    }
+}
