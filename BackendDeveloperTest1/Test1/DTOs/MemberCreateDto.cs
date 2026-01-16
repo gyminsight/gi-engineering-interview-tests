2d9305f0471162f1bb607f525d4d44a862c53d3a
Adolfo Fernández Lazo <fitolazoff@gmail.com>
2026-01-15 22:18:24 -0800

    - Creating Member model and DTOs for handling create, read, and update operations: Member model class, MemberCreateDto, MemberReadDto, MemberUpdateDto

---
+++ BackendDeveloperTest1/Test1/DTOs/MemberCreateDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Test1.DTOs
+{
+    public class MemberCreateDto
+    {
+        
+        public int UID { get; set; }
+        public uint AccountUid { get; set; }
+        public uint LocationUid { get; set; }
+        public bool Primary { get; set; }
+        public DateTime? JoinedDateUtc { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Locale { get; set; }
+        public string PostalCode { get; set; }
+        public bool Cancelled { get; set; }
+        public DateTime? CancelDateUtc { get; set; }
+    }
+}
---
+++ BackendDeveloperTest1/Test1/DTOs/MemberUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Test1.DTOs
+{
+    public class MemberUpdateDto
+    {
+        public Guid Guid { get; set; }
+        public int AccountUid { get; set; }
+        public int LocationUid { get; set; }
+        public bool Primary { get; set; }
+        public DateTime? JoinedDateUtc { get; set; }
+        public DateTime? CancelDateUtc { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Locale { get; set; }
+        public string PostalCode { get; set; }
+        public bool Cancelled { get; set; }
+    }
+}
