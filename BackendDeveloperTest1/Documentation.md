1b9c27341b40b248cb689ce4412a627b2a6ed74d
Sebastian Ross <Sebastian.E.Ross@gmail.com>
2026-01-16 20:48:24 -0800

    Added a members controller with list, create, and delete endpoints.

--- BackendDeveloperTest1/Documentation.md
+++ BackendDeveloperTest1/Documentation.md
@@ -5,7 +5,7 @@ This file documents notes and requested recommendations for the different tasks 
 ## Task 1: 
 Create an accounts controller with list, create, read, update and delete endpoints.
 	- UID of locations was not an element in the LocationDto. Accounts are associated to locations by the location's UID. This causes an issue when creating a new account, as the location's UID is required but can't be easily accessed. Added UID as a field in the LocationDto class as a workaround.
-	- It is unclear what the account.PendCancel field is or how it's used.
+	- It is unclear what the account.PendCancel field is or how it's used. I recommend explaining it in documentation.
 
 ## Task 2:
 Enhance the location's list endpoint to return the number of non-cancelled accounts (where the `account.Status` < [CANCELLED](Test1/Models/AccountStatusType.cs)) for each location.
@@ -15,4 +15,12 @@ Enhance the location's list endpoint to return the number of non-cancelled accou
 Add an endpoint to the accounts controller that will return all members for a specified account (using the account's Guid)
 	- Creating a Members controller and MemberDto class to make handling members easier
 	- Note that member.AccountUID and member.LocationUID are different types than account.UID and location.UID (unsigned int vs integer). This might cause problems if there are a very large amount of accounts or locations.
+
+## Task 4:
+Create a members controller that will list, create, and delete members. 
+   * The create endpoint should only allow for one primary member per account.
+   * The delete endpoint should make the next member on the account the primary member, when the primary member is deleted for an account.  The endpoint should not allow deletion of the last member on the account.
+	- Keeping track of whether a member is primary inside each member is prone to mistakes and overhead. I recommend making a new relationship table to keep track of what the primary member is for each account.
+	- Keeping redundant data about the location inside each member entry *and* each account entry might lead to possible source-of-truth conflicts if a member's location and the member's account's location become different (unless that is somehow intentional). Unless it's intentional, I recommend keeping track of the information only in one place and accessing it by joining queries. If it *is* intentional, I recommend explaining it in documentation.
+	- Using "Primary" as a key in the database is cumbersome and prone to mistakes because "Primary" is a keyword and needs to be escaped every time in order to access the value of the member."Primary" field. I recommend changing the name to something else.
 
--- BackendDeveloperTest1/Test1/Controllers/MembersController.cs
+++ BackendDeveloperTest1/Test1/Controllers/MembersController.cs
@@ -5,10 +5,14 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Security.Principal;
 using Test1.Contracts;
 using Test1.Core;
 using Test1.Models;
+using static Test1.Controllers.AccountsController;
 using static Test1.Controllers.LocationsController;
 
 
@@ -23,6 +27,249 @@ namespace Test1.Controllers
         public MembersController(ISessionFactory sessionFactory)
         {
             _sessionFactory = sessionFactory;
+        }
+
+        // GET: api/members
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> List(CancellationToken cancellationToken)
+        {
+            await using var dbContext = await _sessionFactory.CreateContextAsync(cancellationToken)
+                .ConfigureAwait(false);
+
+            const string sql = @"
+SELECT
+  member.UID,
+  member.Guid,
+  member.AccountUid,
+  member.LocationUid,
+  member.CreatedUtc,
+  member.UpdatedUtc,
+  member.""Primary"",
+  member.JoinedDateUtc,
+  member.CancelDateUtc,
+  member.FirstName,
+  member.LastName,
+  member.Address,
+  member.City,
+  member.Locale,
+  member.PostalCode,
+  member.Cancelled
+FROM member;";
+
+            var builder = new SqlBuilder();
+
+            var template = builder.AddTemplate(sql);
+
+            var rows = await dbContext.Session.QueryAsync<MemberDto>(template.RawSql, template.Parameters, dbContext.Transaction)
+                .ConfigureAwait(false);
+
+            dbContext.Commit();
+
+            return Ok(rows); // Returns an HTTP 200 OK status with the data
+        }
+
+        // POST: api/members
+        [HttpPost]
+        public async Task<ActionResult<string>> Create([FromBody] MemberDto model, CancellationToken cancellationToken)
+        {
+            await using var dbContext = await _sessionFactory.CreateContextAsync(cancellationToken)
+                .ConfigureAwait(false);
+
+            const string primaryCheckSql = @"
+SELECT
+    member.Guid
+FROM member
+/**where**/;";
+
+            var primaryCheckBuilder = new SqlBuilder();
+
+            var primaryCheckTemplate = primaryCheckBuilder.AddTemplate(primaryCheckSql);
+
+            primaryCheckBuilder.Where("member.AccountUid = @AccountUid AND member.\"Primary\" > 0", new
+            {
+                model.AccountUid
+            });
+
+            var primaryCheckRows = await dbContext.Session.QueryAsync<MemberDto>(primaryCheckTemplate.RawSql, primaryCheckTemplate.Parameters, dbContext.Transaction)
+                .ConfigureAwait(false);
+
+            bool primaryExists = primaryCheckRows.Any();
+
+            const string sql = @"
+INSERT INTO member (
+  Guid,
+  AccountUid,
+  LocationUid,
+  CreatedUtc,
+  UpdatedUtc,
+  ""Primary"",
+  JoinedDateUtc,
+  CancelDateUtc,
+  FirstName,
+  LastName,
+  Address,
+  City,
+  Locale,
+  PostalCode,
+  Cancelled
+) VALUES (
+  @Guid,
+  @AccountUid,
+  @LocationUid,
+  @CreatedUtc,
+  @UpdatedUtc,
+  @Primary,
+  @JoinedDateUtc,
+  @CancelDateUtc,
+  @FirstName,
+  @LastName,
+  @Address,
+  @City,
+  @Locale,
+  @PostalCode,
+  @Cancelled
+);";
+
+            var builder = new SqlBuilder();
+
+            DateTime? UpdatedUtc = null;
+            DateTime? CancelDateUtc = null;
+
+            var template = builder.AddTemplate(sql, new
+            {
+                Guid = Guid.NewGuid(),
+                model.AccountUid,
+                model.LocationUid,
+                CreatedUtc = DateTime.Now,
+                UpdatedUtc,
+                Primary = (primaryExists) ? 0: 1, // If there's already a primary member for the specified account, don't make this new member primary. If there isn't, make it primary.
+                JoinedDateUtc = DateTime.Now,
+                CancelDateUtc,
+                model.FirstName,
+                model.LastName,
+                model.Address,
+                model.City,
+                model.Locale,
+                model.PostalCode,
+                Cancelled = 0
+            });
+
+            var count = await dbContext.Session.ExecuteAsync(template.RawSql, template.Parameters, dbContext.Transaction)
+                .ConfigureAwait(false);
+
+            dbContext.Commit();
+
+            if (count == 1)
+                return Ok();
+            else
+                return BadRequest("Unable to add member");
+        }
+
+        // DELETE: api/members/{Guid}
+        [HttpDelete("{id:Guid}")]
+        public async Task<ActionResult<MemberDto>> DeleteById(Guid id, CancellationToken cancellationToken)
+        {
+            await using var dbContext = await _sessionFactory.CreateContextAsync(cancellationToken)
+                .ConfigureAwait(false);
+
+            const string primaryCheckSql = @"
+SELECT
+    delmember.UID,
+    delmember.Guid,
+    delmember.AccountUid,
+    delmember.LocationUid,
+    delmember.""Primary"",
+    COUNT(accountmember.AccountUid = delmember.AccountUid) AS AccountMembersCount
+FROM member AS delmember
+JOIN member AS accountmember
+    ON accountmember.AccountUid = delmember.AccountUid
+    AND delmember.Guid = @Guid;";
+
+            var primaryCheckBuilder = new SqlBuilder();
+
+            var primaryCheckTemplate = primaryCheckBuilder.AddTemplate(primaryCheckSql);
+
+            primaryCheckBuilder.Where("Guid = @Guid", new
+            {
+                Guid = id
+            });
+
+            var primaryCheckRows = await dbContext.Session.QueryAsync<AccountMemberDto>(primaryCheckTemplate.RawSql, primaryCheckTemplate.Parameters, dbContext.Transaction)
+                .ConfigureAwait(false);
+
+            AccountMemberDto delMember = primaryCheckRows.FirstOrDefault();
+
+            if (delMember.Primary > 0) // if the specified member is the account's primary
+            {
+                if (delMember.AccountMembersCount < 2) // and if there isn't at least one other member on this account
+                {
+                    return BadRequest("Cannot delete last member of account");
+                }
+                else // but instead if there's at least one other member on this account
+                {
+                    // update the next member of this account to be primary
+                    const string primaryUpdateSql = @"
+UPDATE member
+SET ""Primary"" = 1
+WHERE UID = (
+    SELECT
+        UID
+    FROM member
+    WHERE
+        AccountUid = @AccountUid
+        AND ""Primary"" < 1
+    ORDER BY
+        CreatedUtc ASC,
+        UID ASC
+    LIMIT 1
+)";
+
+                    var primaryUpdateBuilder = new SqlBuilder();
+
+                    // Set values of the fields for the chosen account to match those specified in the request,
+                    //   but retain current values for fields not specified in the request (i.e., unspecified or null).
+                    var primaryUpdateTemplate = primaryUpdateBuilder.AddTemplate(primaryUpdateSql, new
+                    {
+                        delMember.AccountUid
+                    });
+
+                    var primaryUpdateCount = await dbContext.Session.ExecuteAsync(primaryUpdateTemplate.RawSql, primaryUpdateTemplate.Parameters, dbContext.Transaction)
+                        .ConfigureAwait(false);
+                }
+            }
+
+            // If we've arrived at this point then either:
+            //   a) The member chosen for deletion was not primary
+            //   or b) The chosen member was primary, and we just set the successor to be primary when this one is gone
+            // Either way, it's safe to delete the chosen member now.
+            const string sql = "DELETE FROM member WHERE Guid = @Guid;";
+
+            var builder = new SqlBuilder();
+
+            var template = builder.AddTemplate(sql, new
+            {
+                Guid = id
+            });
+
+            var count = await dbContext.Session.ExecuteAsync(template.RawSql, template.Parameters, dbContext.Transaction)
+                .ConfigureAwait(false);
+
+            dbContext.Commit();
+
+            if (count == 1)
+                return Ok();
+            else
+                return BadRequest("Unable to delete member");
+        }
+
+        public class AccountMemberDto
+        {
+            public int UID { get; set; }
+            public Guid Guid { get; set; }
+            public uint AccountUid { get; set; }
+            public uint LocationUid { get; set; }
+            public short Primary { get; set; }
+            public int AccountMembersCount { get; set; }
         }
 
         public class MemberDto
